3d6b36d8bf0d6747319b7b65fd5adec1caa349c1
Julia Ribeiro <julia_m_ribeiro@estudante.sesisenai.org.br>
2024-03-11 18:50:52 -0300

    Subindo o mini-projeto dos exercicios do trello de 1 a 8

--- semana07/index.js
+++
@@ -1,9 +0,0 @@
-function soma(a) {
-  return function (b) {
-    return a + b;
-  };
-}
-
-const add5 = soma(5);
-
-console.log(add5(2));
---
+++ semana07/mini-projeto.js
@@ -0,0 +1,85 @@
+// Exercicio 1
+class Produto {
+  nome;
+  preco;
+  quantidade;
+
+  // Exercicio2
+  constructor(valorNome, valorPreco, valorQuantidade) {
+    this.nome = valorNome;
+    this.preco = valorPreco;
+    this.quantidade = valorQuantidade;
+  }
+
+  //Exercicio3
+  Vender(quantidadeVendida) {
+    if (quantidadeVendida > this.quantidade) {
+      console.log("Estoque Insuficiente");
+      console.log(`Existem apenas ${this.quantidade} unidades disponíveis.`);
+      return;
+    }
+    this.quantidade -= quantidadeVendida;
+  }
+
+  // Exercicio4
+  Repor(quantidadeReposta) {
+    this.quantidade += quantidadeReposta;
+  }
+
+  // Exercicio5
+  MostrarEstoque() {
+    console.log(
+      `O produto ${this.nome} custa R$${this.preco} e possui ${this.quantidade} unidades disponíveis.`
+    );
+  }
+
+  //Exercicio6
+  AtualizarPreco(novoValor) {
+    this.preco = novoValor;
+  }
+}
+
+// Exercicio7
+class Pessoa {
+  nome;
+  idade;
+  profissao;
+
+  constructor(nome, idade, profissao) {
+    this.nome = nome;
+    this.idade = idade;
+    this.profissao = profissao;
+  }
+}
+
+// Exercicio8
+class Cliente extends Pessoa {
+  telefone;
+  email;
+  clienteDesde;
+
+  constructor(nome, idade, profissao, telefone, email, clienteDesde) {
+    super(nome, idade, profissao);
+    this.telefone = telefone;
+    this.email = email;
+    this.clienteDesde = clienteDesde;
+  }
+}
+
+/* #### TESTE #### */
+const produto1 = new Produto("Camiseta", 20, 50);
+produto1.Repor(10);
+produto1.Vender(5);
+produto1.Repor(2);
+produto1.AtualizarPreco(15);
+produto1.MostrarEstoque();
+
+const cliente = new Cliente(
+  "Julia",
+  28,
+  "Programadora",
+  "88999999",
+  "email@email.com",
+  "2015-03-28"
+);
+console.log(cliente);
